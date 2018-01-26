ede850606470d4d6d4473548d2e01e4c67c3162e
TasiYokan <guanghao.yang@outlook.com>
2018-01-26 10:22:16 -0500

    Update audio module and integrate bgm

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TasiYokan.Audio;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        print(AudioManager.Instance);
+        new SingleAudio("BGM_LOOP").OnStart(() =>
+        {
+            print("hi");
+        }).Play();
+
+        int a = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
